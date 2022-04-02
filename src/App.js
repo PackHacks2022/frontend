85305ab0870750faebbcdddf095c67b6306395dc
Joydeep Mukherjee <joydeepm02@gmail.com>
2022-04-02 16:20:57 -0700

    Fetch backend API example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [course, setCourse] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/course/1", {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json()).then(data => {
+      setCourse(data);
+    })
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {course &&
+      <div>
+        <h1>Fetch course:</h1>
+        <p>{course.department}</p>
+        <p>{course.number}</p>
+        <p>{course.title}</p>
+      </div>}
     </div>
   );
 }
